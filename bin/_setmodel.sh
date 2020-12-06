f9b97c423d0a1a705cb130669d8da86ab5aeaeae
JGWill <jgi@jgwill.com>
2020-12-05 21:40:20 -0500

    import: Training on GPU Docker PLatform progress
    * The run-gpu-train-bg Script mount local directory with required path (data_large, style dataset and model target dir (to save the checkpoints))
    * Dockerfile creates the necessary container layers to run on Linux (untested on windows)  ./build.sh builds it
    * cezanne.sh is to try infering an image from the downloaded image (untested MEmory limits because the model is training)
    * run-gpu - Runs the model on a GPU
    
    # Prereq
    
    * NVidia-Docker2
    * Docker
    * GPU Hardware
    * Downloaded [Places365-Standard high-res train mages (105GB)](http://data.csail.mit.edu/places/places365/train_large_places365standard.tar)
    
    Thanks to Artsiom Sanakoyeu, Dmytro Kotovenko

---
+++ bin/_setmodel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+source _env.sh
+
+export train_script=./trainer.sh
+
+export model_context_name=model_$1_new
+
+export modelbasename=$1
+export lib_namespace=$1
+
+echo "Model context: $model_context_name"
+echo "Modelbasename: $modelbasename"
+
+#model_local_fullpath
+
+export model_rel_path=model/models/$model_context_name
+export model_local_fullpath=$model_local_dir/$model_rel_path
+
---
+++ bin/exec.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Connecting to docker..."
+
+docker exec -it 2656b7e868d3 bash
+
+
