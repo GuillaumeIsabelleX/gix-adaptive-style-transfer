98e33e67e01ce04b8ffdea9f6552706a4193e991
JGWill <jgi@jgwill.com>
2020-12-19 19:47:11 -0500

    dos2unix

--- bin-gpu/_fori_env.sh
+++ bin-gpu/_fori_env.sh
@@ -1,17 +1,19 @@
 
 #
 
-#export chks="15 30 45 60 75 90 105 120 135 150 165 180 195 210 225 240 255 270 285 300"
-export chks="225 150 120 255 270 285"
+export chks="15 30 45 60 75 90 105 120 135 150 165 180 195 210 225 240 255 270 285 300"
+#export chks="225 150 120 255 270 285"
 #export chks="30 75 120 150 225 240 255 270 285 300"
 export img_res=2900
 export suffix="__"
 export suffixseparator="__"
 export model=model_gia-young-picasso-v03-201216_new
-export content=/a/lib/samples/content2012181444
-export savedir=/a/lib/results/it-chkp
+#export content=/a/lib/samples/content2012181444
+export content=/a/lib/datasets/paintergansets/paul-cezanne
+export savedir=/a/lib/results/it-chkp/paul-cezanne
+#export savedir=/a/lib/results/it-chkp
 export script=/work/i4.sh
-export subdir=2_9k
+export subdir=1k
 export savefullpath=$savedir/$subdir
 export ind=index.md
 
