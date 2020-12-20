59b17a70c9897f1edc3f733eace5a84c11e88b1f
JGWill <jgi@jgwill.com>
2020-12-19 19:38:39 -0500

    add:sample

--- bin-gpu/sample-i4.sh
+++ bin-gpu/sample-i4.sh
@@ -1,1 +1,2 @@
 ./i4.sh model_gia-young-picasso-v03-201216_new /subdir __suffix 1024 /a/lib/samples/content /a/lib/results/someoutput 285000
+nvidia-docker run -it --rm --memory=3190093888 --memory-swap=-1 -v /a/gix-adaptive-style-transfer/bin-gpu:/work -v /a/lib:/a/lib -v /a/model/models:/model/models -e DISPLAY=10.10.22.143:0.0 jgwill/gix-adaptive-style-transfer:gpu
