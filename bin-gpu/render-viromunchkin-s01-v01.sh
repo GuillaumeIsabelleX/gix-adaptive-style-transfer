4ab40e925cd7a6ca19f25069fb9566354857e3d6
JGWill <jgi@jgwill.com>
2021-04-20 18:42:25 -0400

    update

--- bin-gpu/render-viromunchkin-s01-v01.sh
+++ bin-gpu/render-viromunchkin-s01-v01.sh
@@ -2,18 +2,15 @@
 
 export savedirnamespace=viromunchkin-s01-v01_new
 export dimloop="256 512 768 1024 1280 1536 1792 2048 2304 2560 2816"
-export dimloop="256 512 768 1024"
+export dimloop="256 512 768 1024 1280 1536 1792"
 export chks_long_all="15 30 45 60 75 90 105 120 135 150 165 180 195 210 225 240 255 270 285 300"
-export chks_long_all="15 30 45 60 75 90 105 120 135 150 165 180"
 export partial_210108="15 30 45 60 75 90"
-export partial_210108="15 30 45 60 75 90 105 120 135 150 1165 180 195"
-export partial_210108="195 210 225 240 255 270 285 300"
 export partial_210108="15"
 
 #iteration 2012270346 - IMG_0019 choosen
 #export chks="30 105 150 225 240 270 285 300"
 export chks=$chks_long_all
-export chks=$partial_210108
+#export chks=$partial_210108
 
 
 #contentpath=/a/lib/samples/ap__210217
