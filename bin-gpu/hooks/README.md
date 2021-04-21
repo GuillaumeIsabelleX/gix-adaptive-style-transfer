87147b88b24b8a828bdfe0f1581ead933da6626b
JGWill <jgi@jgwill.com>
2021-04-21 08:11:15 -0400

    doc:added doc
    * one hook and one msg
    
    ## [bashrc-entering-container.sh](bashrc-entering-container.sh)
    
    Runs when entering the container.
    
    ## [entering-msg.txt](entering-msg.txt)
    
    * catted file when login.
    * Show ASCII art :)
    * Show relevant information (credits and stuff)
    
    feature #9

--- bin-gpu/hooks/README.md
+++ bin-gpu/hooks/README.md
@@ -1,3 +1,9 @@
+
+- [Intentions](#intentions)
+- [Assumptions](#assumptions)
+- [Hooks](#hooks)
+  - [bashrc-entering-container.sh](#bashrc-entering-containersh)
+  - [entering-msg.txt](#entering-msgtxt)
 # Intentions
 
 * Hook when stuff arrives in container
@@ -6,4 +12,15 @@
 
 * All container mount the current directory context in /work so /work/hooks in always the same path
 
+# Hooks
+
+## [bashrc-entering-container.sh](bashrc-entering-container.sh)
+
+Runs when entering the container.
+
+## [entering-msg.txt](entering-msg.txt)
+
+* catted file when login.
+* Show ASCII art :)
+* Show relevant information (credits and stuff)
 
--- bin-gpu/hooks/bashrc-entering-container.sh
+++ bin-gpu/hooks/bashrc-entering-container.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
 # Run when entering bashrcA
-echo "
+source /work/_env.sh
+cat $hb/entering-msg.txt
 
-                                                                                  
-       db               ad88888ba   888888888888         db              88       
-             d88b             d8"     "8b       88             d88b             88       
-	          d8'`8b            Y8,               88            d8'`8b            88       
-		      d8'  `8b           `Y8aaaaa,         88           d8'  `8b           88       
-		         d8YaaaaY8b            `"""""8b,       88          d8YaaaaY8b          88       
-			   d8""""""""8b                 `8b       88         d8""""""""8b         88       
-			    d8'        `8b   888  Y8a     a8P  888  88  888   d8'        `8b   888  88  888  
-			    d8'          `8b  888   "Y88888P"   888  88  888  d8'          `8b  888  88  888  
-			                                                                                      
-			                                                                                      
-
-  
-	      "
 echo "------------------------------------"
 echo "Rendering Script sample are in /work"
