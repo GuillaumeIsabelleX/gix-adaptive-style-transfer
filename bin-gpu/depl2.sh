b74db254b9d14f32c1c6da232ff107684bef71af
JGWill-Orko <jgi@jgwill.com>
2021-05-15 09:08:53 -0400

    ren

--- bin-gpu/depl2.sh
+++ bin-gpu/depl2.sh
@@ -21,7 +21,7 @@ cmd="$docker_exec run -it  -v $(pwd):$container_workdir \
 	-v $lib_root:$docker_lib_root \
 	-v  $model_root/models:$container_models_fullpath \
 	-e DISPLAY=$DISPLAY \
-	$container_tag
+	$container_tag $@
 	"
 echo "-----------------The following command will ran :"
 echo $cmd
