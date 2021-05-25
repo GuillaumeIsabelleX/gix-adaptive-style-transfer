204d54dd908740ff2aff6be2a7d61ba865bffd51
JGWill-Orko <jgi@jgwill.com>
2021-05-25 13:38:35 -0400

    x: Higher reso dev

--- stcgoal_higher_reso_dev.sh
+++ stcgoal_higher_reso_dev.sh
@@ -2,7 +2,7 @@
 
 #@STCgoal Render PNG (highest quality than JPG)  
 #@a This makes a model to render locally in a container
-
+#@state The wmodel is our current repo branch with 
  cd /wmodel/ && \
      mkdir /m && \
      tar cf - * | (cd /m; tar xf -) && \
@@ -13,4 +13,4 @@
      mv model_png.py model.py && \
      echo "All set for infering PNG" && \
      ./inference.sh
-
+#@state Necessary files were copied, moved and inferences starts
