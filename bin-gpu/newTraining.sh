b6630959fd0268ca362a0e21545b42af7e9f8657
JGWill-Orko <jgi@jgwill.com>
2021-05-31 22:31:25 -0400

    fix

--- bin-gpu/newTraining.sh
+++ bin-gpu/newTraining.sh
@@ -23,28 +23,31 @@ export PATTERN="DSNAME"
 export PATTERN="FTAG"
 
 cd $cdir
-tfile=$sdir/train-$ds.sh
-traintargetfile=$tfile
+traintargetfile=train-$ds.sh
+tfile=$sdir/$traintargetfile
+
 if [ ! -e "$tfile" ]; then #@state do process, we are new
 	cp $traintemplate $tfile && \
  	sed -i 's/DSNAME/'"$ds"'/g' $tfile && \
-	  sed -i 's/FTAG/'"$ftag"'/g' && msg_status "Train file completed $tfile" 0 || \
-	  (msg_status "Failed to prep $tfile" 1   && exit 1)
+	  sed -i 's/FTAG/'"$ftag"'/g'  $tfile && msg_status "Train file completed $tfile" SUCCESS || \
+	  (msg_status "Failed to prep $tfile" FAILED   && exit 1)
 
  fi
  cd $cdir
-tfile=$sdir/_render_env_$ds.sh
-rendertargetfile=$tfile
+
+
+rendertargetfile=_render_env_$ds.sh
+tfile=$sdir/$rendertargetfile
 if [ ! -e "$tfile" ]; then #@state do process, we are new
 	cp $rendertemplate $tfile && \
  	sed -i 's/DSNAME/'"$ds"'/g' $tfile && \
-          sed -i 's/FTAG/'"$ftag"'/g' && msg_status "Render file completed $tfile" 0 || \
-	  (msg_status "Failed to prep $tfile" 1 && exit 1)
+          sed -i 's/FTAG/'"$ftag"'/g'  $tfile && msg_status  "Render file completed $tfile" SUCCESS || \
+	  (msg_status "Failed to prep $tfile" FAILED && exit 1)
 
 fi
 
-echo "train :  cd $cdir; ./$traintargetfile"
-echo "render: cd $cdir; ./doit.sh $rendertargetfile 15 30 45 ..."
+echo "train :  ./$traintargetfile"
+echo "render:  ./doit.sh $rendertargetfile 15 30 45 ..."
 echo "# $ftag : $ds"  >> .bash_aliases
 echo "alias train$ftag=\"./$traintargetfile\"" >> .bash_aliases
 echo "alias render$ftag=\"./doit.sh $rendertargetfile \""  >> .bash_aliases
