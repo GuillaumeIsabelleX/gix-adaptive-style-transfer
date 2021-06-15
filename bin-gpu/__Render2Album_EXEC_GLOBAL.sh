4ce7b6640e8ba24f3309c41aaea02db89c5c3f01
JGWill-Orko <jgi@jgwill.com>
2021-06-15 09:50:02 -0400

    chg: with a Master EXEC GLOBAL

--- bin-gpu/__Render2Album_EXEC_GLOBAL.sh
+++ bin-gpu/__Render2Album_EXEC_GLOBAL.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 if [ -e $binroot/__fn.sh ] && [ "$FNLOADED" == "" ]; then
    source $binroot/__fn.sh $@
 else 
@@ -7,8 +8,15 @@ else
 	exit 1
 fi
 
+
 export LOG_ENABLED=y
 DEBUG=0
+log_info "Starting $0"
+log_status "$renderbatch" CONFIG
+log_status "$ds" DS
+log_status "$ftag" FTAG
+
+executer=./doit.sh-cpu
 
 renderToMnotageToPipelineEndingByCloudPublished="$binroot/result_To_Montage_Pipeline-To_cloudNotified.sh"
 #csmForeachFolderInCurrent="$binroot/_csm_foreach_dir_in_current.sh"
@@ -17,12 +25,15 @@ export tcloudroot=/home/jgi/astiapreviz
 
 export tcloudgetaddress=/home/jgi/astiapreviz/_getaddress.sh
 
+
+log_status "$modeltag" MODELTAG
+
 export tclouddir=$tcloudroot/$ftag
 mkdir -p $tclouddir
-
+#export savedirnamespace=${ds//"gia-ds-"/}
 export savefulldir=$libroot/results/$savedirnamespace/$ftag
 
-
+dvar ds modelname modeltag ftag savedirnamespace savefulldir
 cloudit() {
 	local f="$1"
 	(cp $f $tclouddir && \
@@ -30,7 +41,13 @@ cloudit() {
 		$tcloudgetaddress $ftag/$f --sns && echo "done adding $f to $tclouddir")
 
 }
-
+#@a Rendering is completed, \
+	#@a then a contact sheet is generated and published with a URL as notification \
+	#@a then separated into content \
+	#@a then a Montage with content and original \
+	#@a then made into an album
+	#@a then published to the cloud
+	#@a then an address is generated and send as notification thru email
 log_status "Rendering" STARTING && \
 	$executer $renderbatch $chks && \
 	log_status "Rendering" COMPLETED && \
