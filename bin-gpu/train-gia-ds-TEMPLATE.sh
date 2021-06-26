fdf62d10906df562d890f8e334493fe86dee89fc
JGWill-Orko <jgi@jgwill.com>
2021-06-26 02:29:38 -0400

    fix

--- bin-gpu/train-gia-ds-TEMPLATE.sh
+++ bin-gpu/train-gia-ds-TEMPLATE.sh
@@ -10,9 +10,9 @@ ds=DSNAME
 
 # CHOOSE THE DESIRED TRAINER
 
-./run-gpu-train-bg-more-chk-higher-res-928x $ds  #Train in 928x 
+# NOT WORKING TOO ./run-gpu-train-bg-more-chk-higher-res-928x $ds  #Train in 928x 
 # LIMITED NOT WORKING ON CURRENT GPU ./run-gpu-train-bg-more-chk-higher-res-1k $ds  #Train in 1024x (dataset must be in 1k)
-#./run-gpu-train-bg-more-chk-higher-res $ds    #Train in 864x
+./run-gpu-train-bg-more-chk-higher-res $ds    #Train in 864x
 #./run-gpu-train-bg-more-chk $ds	       #Train regular but more checkpoints
 #./run-gpu-train-bg $ds
 #./run-gpu-train-bg-var2 $ds
