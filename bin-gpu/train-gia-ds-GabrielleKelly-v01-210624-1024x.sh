cd55a0e8bba1e4461c90c7465c1f337793c77087
JGWill-Orko <jgi@jgwill.com>
2021-06-26 02:16:14 -0400

    chg:down-res

--- bin-gpu/train-gia-ds-GabrielleKelly-v01-210624-1024x.sh
+++ bin-gpu/train-gia-ds-GabrielleKelly-v01-210624-1024x.sh
@@ -10,7 +10,8 @@ ds=gia-ds-GabrielleKelly-v01-210624-1024x
 
 # CHOOSE THE DESIRED TRAINER
 
-./run-gpu-train-bg-more-chk-higher-res-1k $ds  #Train in 1024x (dataset must be in 1k)
+./run-gpu-train-bg-more-chk-higher-res-928x $ds  #Train in 928x
+
 #./run-gpu-train-bg-more-chk-higher-res $ds    #Train in 864x
 #./run-gpu-train-bg-more-chk $ds	       #Train regular but more checkpoints
 #./run-gpu-train-bg $ds
